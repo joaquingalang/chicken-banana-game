3c5510dbd088e8ab2a3790e6095e67c199a72abe
joaquingalang <galangjoaquin@gmail.com>
2025-07-02 02:10:48 +0800

    Add: game status component

---
+++ src/components/GameStatus.tsx
@@ -0,0 +1,13 @@
+
+
+function GameStatus() {
+  return <div className="stat-board">
+    <p className="stat-title">GAME STATUS</p>
+    <p className="player-desc">Two players: Chicken & Banana</p>
+    <div className="turn-taker chicken">
+        <p>Chicken's Turn</p>
+    </div>
+  </div>;
+}
+
+export default GameStatus;
