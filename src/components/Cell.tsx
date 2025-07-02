c2dc33e70b0073d2ca75a274f3869af6c620202f
joaquingalang <galangjoaquin@gmail.com>
2025-07-02 10:30:40 +0800

    Update: reveal image logic

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,12 +1,39 @@
+import { useState } from "react";
+import chicken from "../../public/chicken.webp";
+import banana from "../../public/banana.webp";
 
 interface Props {
-    index: number,
+  index: number;
+  type: "chicken" | "banana";
+  onClick: () => void;
 }
 
-function Cell({index}: Props) {
-    return (
-        <div className="cell">{index}</div>
-    );
+function Cell({ index, type, onClick }: Props) {
+  let [isClicked, setIsClicked] = useState(false);
+
+  const getImage = (type: string) => {
+    const image = type === "chicken" ? chicken : banana;
+    return <img height="50" src={image} alt="type" />;
+  };
+
+  return (
+    <div
+      className={
+        "cell" +
+        (isClicked && type === "chicken"
+          ? " chicken"
+          : isClicked && type === "banana"
+          ? " banana"
+          : "")
+      }
+      onClick={() => {
+        onClick();
+        setIsClicked(true);
+      }}
+    >
+      {isClicked ? getImage(type) : index}
+    </div>
+  );
 }
 
 export default Cell;
