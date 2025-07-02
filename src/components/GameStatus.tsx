6e2ffad9225aba851d64709b71a05f4a41b993c1
joaquingalang <galangjoaquin@gmail.com>
2025-07-02 20:41:33 +0800

    Update: winner message

--- src/components/GameStatus.tsx
+++ src/components/GameStatus.tsx
@@ -1,14 +1,18 @@
 
 interface Props {
   turnTaker: string,
+  chickenWins: boolean,
+  bananaWins: boolean,
 }
 
-function GameStatus({turnTaker}: Props) {
+function GameStatus({turnTaker, chickenWins, bananaWins}: Props) {
   return <div className="stat-board">
     <p className="stat-title">GAME STATUS</p>
     <p className="player-desc">Two players: Chicken & Banana</p>
     <div className={"turn-taker " + turnTaker}>
-        <p>{(turnTaker === "chicken" ? "Chicken" : "Banana") + "'s Turn"}</p>
+      {(chickenWins || bananaWins) ? null : <p>{(turnTaker === "chicken" ? "Chicken" : "Banana") + "'s Turn"}</p>}
+      {chickenWins && <p>🏆 Chicken Wins! 🎉</p>}
+      {bananaWins && <p>🏆 Banana Wins! 🎉</p>}
     </div>
   </div>;
 }
