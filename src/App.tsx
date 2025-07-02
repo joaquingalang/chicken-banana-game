1c7d891d3245b6f87be8bcce252108bf9a584a29
joaquingalang <galangjoaquin@gmail.com>
2025-07-02 20:41:49 +0800

    Update: cell clickable status and reset button

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,13 @@ function App() {
   // State Variables
   const chickens = Array(18).fill("chicken");
   const banana = Array(18).fill("banana");
-  const [gameCells] = useState(() => shuffle([...chickens, ...banana]));
+  const [gameCells, setGameCells] = useState(() => shuffle([...chickens, ...banana]));
   let [turnTaker, setTurnTaker] = useState("chicken");
   let [chickenScore, setChickenScore] = useState(0);
   let [bananaScore, setBananaScore] = useState(0);
+  let [chickenWins, setChickenWins] = useState(false);
+  let [bananaWins, setBananaWins] = useState(false);
+  let [isGameOver, setIsGameOver] = useState(false);
 
   // Functions
   const switchTurns = (turnTaker: string) => {
@@ -29,13 +32,26 @@ function App() {
 
   const checkAnswer = (turnTaker: string, value: string) => {
     if (turnTaker === value) {
+      switchTurns(turnTaker);
       if (value === "chicken") {
         setChickenScore(chickenScore + 1);
       } else {
         setBananaScore(chickenScore + 1);
       }
+    } else {
+      setIsGameOver(true);
+      switchTurns(turnTaker);
+      if (turnTaker === "chicken") {
+        setBananaWins(true);
+      } else {
+        setChickenWins(true);
+      }
     }
-  }
+  };
+
+  const resetGame = () => {
+    window.location.reload();
+  };
 
   return (
     <>
@@ -45,7 +61,11 @@ function App() {
             chickenScore={chickenScore}
             bananaScore={bananaScore}
           ></Scoreboard>
-          <GameStatus turnTaker={turnTaker}></GameStatus>
+          <GameStatus
+            turnTaker={turnTaker}
+            chickenWins={chickenWins}
+            bananaWins={bananaWins}
+          ></GameStatus>
           <div className="game-status"></div>
         </div>
         <div className="game-grid">
@@ -53,14 +73,15 @@ function App() {
             <Cell
               index={index + 1}
               type={value}
+              isClickable={!isGameOver}
               onClick={() => {
                 checkAnswer(turnTaker, value);
-                switchTurns(turnTaker);
                 console.log("TEST");
               }}
             ></Cell>
           ))}
         </div>
+        {isGameOver && <button className="reset-button active" onClick={resetGame}>🔄 Reset</button>}
       </div>
     </>
   );
